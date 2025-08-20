7e4ed4badec003df6907f84a766d2715056d6e76
Ethan Froidevaux <ethan.froidevaux@divtec.ch>
2025-08-20 10:56:53 +0200

    Ajout fonction pour relancer dé (pas fini)

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,29 +1,72 @@
-public class YahtzeeProcedural {
+import java.util.Scanner;
 
-    public static int lancerDe (){
-        return (int) (Math.random() * 6 + 1);
+public class YahtzeeProcedural {
+    public static final int NOMBRE_FACE_DE = 6;
+    public static final int NOMBRE_DES_A_LANCER = 5;
+
+    /**
+     * Lance un dé
+     * @return un nombre aléatoire compris entre 1 et 6
+     */
+    public static int lancerDe() {
+        return (int) (Math.random() * NOMBRE_FACE_DE + 1);
     }
 
-    public static void lancer5Des(int[] mesDes){
+    /**
+     * Lance plusieurs dés
+     * @param mesDes Liste dans laqelle sont stockés les dés
+     */
+    public static void lancerPlusieursDes(int[] mesDes) {
         for (int i = 0; i < mesDes.length; i++) {
             int des = lancerDe();
             mesDes[i] = des;
         }
     }
 
-    public static void afficherDes(int[] mesDes){
+    /**
+     * Affiche les dés contenu dans le tableau
+     * @param mesDes Liste dans laqelle sont stockés les dés
+     */
+    public static void afficherDes(int[] mesDes) {
         for (int i = 0; i < mesDes.length; i++) {
             System.out.print("[" + (i + 1) + "]" + mesDes[i] + "\t");
         }
     }
 
-    public static void main(String[] args) {
-        int[] mesDes = new int[5];
-        lancer5Des(mesDes);
-        afficherDes(mesDes);
+    /**
+     * Permet de relancer certain dé
+     * @param mesDes
+     */
+    public static void relancerDes(int[] mesDes) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voulez vous relancer un dé ? (o/n) ");
+        String lettreSaisie = scanner.next();
+
+        // Vérifie la lettre saisie
+        if (lettreSaisie.equals("o") || lettreSaisie.equals("O")) {
+            System.out.print("\nDé à relancer : ");
+            int nombreSaisi =  scanner.nextInt();
+
+        }
+
+
+
+
+
+        //System.out.println("Veuillez saisir un nombre entre 1 et " + mesDes.length);
+
+        //mesDes[nombreSaisi - 1] = 9;
+
+
     }
 
+    public static void main(String[] args) {
+        int[] mesDes = new int[NOMBRE_DES_A_LANCER];
+        lancerPlusieursDes(mesDes);
+        afficherDes(mesDes);
+        relancerDes(mesDes);
 
-
-    //test
+        afficherDes(mesDes);
+    }
 }
+
