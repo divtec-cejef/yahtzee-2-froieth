2732c1290edc1b4f1aad90d2e4f97b612e3d6acc
Ethan Froidevaux <ethan.froidevaux@divtec.ch>
2025-08-20 09:57:48 +0200

    Réorganisation du premier jallon + correcion

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -2,32 +2,27 @@ import java.util.Collections;
 
 public class YahtzeeProcedural {
 
-    public class lancerDes  {
+    public static int lancerDe (){
+        return (int) (Math.random() * 6 + 1);
+    }
 
-        public int lancerDe (){
-            return (int) (Math.random() * 6 + 1);
+    public static int[] lancer5Des(int[] mesDes){
+        for (int i = 0; i < mesDes.length; i++) {
+            int des = lancerDe();
+            mesDes[i] = des;
         }
+        return mesDes;
+    }
 
-        public int[] lancer5Des(int[] mesDes){
-            for (int i = 0; i < mesDes.length; i++) {
-                int des = lancerDe();
-                mesDes[i] = des;
-            }
-            return mesDes;
+    public static void afficherDes(int[] mesDes){
+        for (int i = 0; i < mesDes.length; i++) {
+            System.out.print("[" + (i + 1) + "]" + mesDes[i] + "\t");
         }
-
-        public void afficherDes(int[] mesDes){
-            for (int i = 0; i < mesDes.length; i++) {
-                System.out.println("[" + (i + 1) + "]" + mesDes[i] + "\t");
-            }
-        }
-
     }
 
     public static void main(String[] args) {
-
-
-
+        int[] mesDes = new int[5];
+        lancer5Des(mesDes);
+        afficherDes(mesDes);
     }
-
 }
