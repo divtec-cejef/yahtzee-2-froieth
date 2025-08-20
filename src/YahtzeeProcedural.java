d168c393021620ef073515113358267a49b5cd7d
Ethan Froidevaux <ethan.froidevaux@divtec.ch>
2025-08-20 09:08:55 +0200

    New - Créer un liste de 5 dés aléatoire

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,21 @@
 public class YahtzeeProcedural {
 
     public static void main(String[] args) {
-        System.out.println( "Bon courage !");
+
+
+        int[] mesDes = new int[5];
+
+        for (int i = 0; i < mesDes.length; i++) {
+            int des = (int) (Math.random() * 6 + 1);
+            mesDes[i] = des;
+        }
+
+
+
+        for (int i = 0; i < mesDes.length; i++) {
+            System.out.println(i + 1 + "\t" + mesDes[i]);
+        }
+
 
     }
 }
